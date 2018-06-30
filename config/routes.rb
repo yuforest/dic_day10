b5c7dc0b240d4ee84778245a08b4f187443df65f
Hajime Yasui <anjineee@gmail.com>
2018-06-30 05:55:12 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+
+  root to: 'users#new'
+  resources :blogs
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+
 end
